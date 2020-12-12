52addcb2d6c0a74bdbd64353d509d41c6438f66a
Daniel Ackermans <ackerd2@rpi.edu>
2020-12-12 14:31:10 -0500

    mid-day work update

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,6 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const query = urlParams.get('showPage')
 
-console.log(query)
-
 if(query == null || query != 'yes'){
 	window.location.href = "./under-construction.html";
 }
